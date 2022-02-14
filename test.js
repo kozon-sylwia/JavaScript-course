42d73c7146baa1cd36a6d5d24fdc84a7ab911bf5
Sylwia Kozon <kozon.sylwia.k@gmail.com>
2022-02-14 19:23:38 +0100

    Changed if statement to while loop in test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var myAge = 15;
-if (myAge < 18 || myAge > 30){
+var age = 5;
 
-    document.write("you aint coming!");
+while (age < 10) {
 
-} else{
-    
-    document.write("You can come, you cool dude");
+        console.log("Your age is less than 10!");
+        age++;
 }
+
+document.write("You are now over 10");
