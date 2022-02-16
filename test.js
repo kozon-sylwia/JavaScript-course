02e6e4bb8c1201eb613ef8eea8cf8dc9b3af541c
Sylwia Kozon <kozon.sylwia.k@gmail.com>
2022-02-16 18:06:52 +0100

    Funtions deleted and math objects added in test.js

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
-function getAverage (a,b) {
+var a = 7;
+var b = 5;
 
-        var average = (a + b) / 6;
-        console.log(average);
-        return average;
+console.log(a+b);
 
-}
-
-var myResult = getAverage(7,11);
-
-
-function logResult (){
-
-        console.log("the average is " + myResult);
-}
-
-logResult()
+console.log(Math.PI);
